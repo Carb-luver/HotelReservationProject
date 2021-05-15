470cb811f541630f7fa6e7827ab341a55fcb1714
Carb-luver <mnt19@pitt.edu>
2021-05-15 18:02:31 -0400

    Initial Commit

---
+++ src/main/java/com/RemoteMode/HotelReservationProject/HotelReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.RemoteMode.HotelReservationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RemoteMode/HotelReservationProject/HotelReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RemoteMode.HotelReservationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
